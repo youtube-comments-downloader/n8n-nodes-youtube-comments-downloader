0ce28b8b9f56c6c161177d0c5b0af6e74c910a5d
Bartek Igielski <igloczek@gmail.com>
2025-12-02 21:36:50 +0100

    init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	tabWidth: 2,
+	useTabs: false,
+	semi: false,
+	singleAttributePerLine: true,
+	vueIndentScriptAndStyle: true,
+	bracketSameLine: false,
+	htmlWhitespaceSensitivity: 'ignore',
+};
